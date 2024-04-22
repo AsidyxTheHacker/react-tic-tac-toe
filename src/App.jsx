71ee1a4c1d40e2647eb884f3763d80d00b99df6f
Adam <adammiller.2000nj@gmail.com>
2024-04-22 15:49:29 -0400

    added TicTacToe.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
 
 function App() {
 
   return (
     <>
-      
+      <div>
+        <TicTacToe />
+      </div>
     </>
   )
-}
+};
 
 export default App;
